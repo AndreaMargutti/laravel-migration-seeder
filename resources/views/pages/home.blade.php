449ae2bfba3aa3f40dd23b38ad1a89ab0946eeb5
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-25 15:50:11 +0200

    add all trains in homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,4 +5,30 @@
 @section("main-content")
     <h1>Treni Disponibili</h1>
     {{-- @dd($trains) --}}
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                @forelse ( $trains as $trains )
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><strong>{{$trains->brand}}</strong></h5>
+                        <p class="card-text">{{$trains->departure_day}}</p>
+                      </div>
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{{$trains->departure_station}} ---> {{$trains->arrival_station}}</li>
+                        <li class="list-group-item">{{$trains->departure_hour}} ---> {{$trains->arrival_hour}}</li>
+                        <li class="list-group-item">{{$trains->train_code}}</li>
+                        <li class="list-group-item">{{$trains->wagon_number}}</li>
+                        <li class="list-group-item">{{$trains->is_punctual}}</li>
+                        <li class="list-group-item">{{$trains->is_canceled}}</li>
+                      </ul>
+                </div>
+                @empty
+                    <div class="col-12">
+                        <h1>No trains available on this date...</h1>
+                    </div>
+                @endforelse
+            </div>
+        </div>
+    </div>
 @endsection
