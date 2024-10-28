6398d6adefbfacc111cee56cbaaa69aec660ce5e
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-28 12:45:46 +0100

    added data

--- database/seeders/PassengerSeeder.php
+++ database/seeders/PassengerSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Passenger;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Testing\Fakes\Fake;
 
 class PassengerSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         //
+        for ($i=0; $i < 10 ; $i++) {
+            # code...
+            $newPassenger = new Passenger();
+            $newPassenger->first_name = $faker->name;
+            $newPassenger->last_name = $faker->name;
+            $newPassenger->age = $faker->numberBetween(18, 50);
+            $newPassenger->save();
+        }
+
     }
 }
--- resources/views/passenger/index.blade.php
+++ resources/views/passenger/index.blade.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Lista Passeggeri</h1>
+
 </body>
 </html>
